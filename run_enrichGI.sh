379df4954d4408fcd510564c64ac204ddbc19515
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2020-11-02 18:15:45 -0500

    defining inputs based on various project types

--- run_enrichGI.sh
+++ run_enrichGI.sh
@@ -5,21 +5,26 @@
 ## 1) crispr_type: one of chemgen (chemical), musgen (mouse), mgin (mouse GI), abgen (antibody), or pace
 ## 2) crispr_path: path to folder where gene scores are stored
 ## 2) crispr_path: name of child folder where gene scores are stored
-crispr_type="mgin"
-crispr_path="/Users/catherineross/projects/GIN/scripts/crispRscore/output"
-crispr_set="20201028_MGIN"
-
-# Specify appropriate gene set annotation file for CRISPR screen type
-if [ $crispr_type = "musgen" ] || [ $crispr_type = "mgin" ]; then
-  annotation_file=input/Mouse_Human_Reactome_April_01_2019_symbol.gmt
-else
-  annotation_file=input/Human_Reactome_October_01_2018_symbol.gmt
-fi
+crispr_type="gin"
+crispr_path="/Users/catherineross/projects/qGI/output_data"
+#crispr_path="/Users/catherineross/projects/GIN/scripts/crispRscore/output"
+crispr_set="20201028"
 
 # I/O
 dt=`date +%Y%m%d`
 indir=${crispr_path}/${crispr_set}
 outdir=output/${crispr_set}
+
+# Specify appropriate gene set annotation file for CRISPR screen type
+if [ $crispr_type = "musgen" ] || [ $crispr_type = "mgin" ]; then
+  #annotation_file=input/Mouse_Human_Reactome_April_01_2019_symbol.gmt
+  annotation_file=input/Mouse_Human_KEGG_December_24_2015_symbol.gmt
+  input_file=${indir}/*_masterTable.txt
+else
+  #annotation_file=input/Human_Reactome_October_01_2018_symbol.gmt
+  annotation_file=input/Human_KEGG_December_24_2015_symbol.gmt
+  input_file=${indir}/qGI_${crispr_set}.txt
+fi
 
 # Generate dated output directory
 if [ -d "$outdir" ]; then
@@ -29,10 +34,9 @@ else
 fi
 
 # Run enrichGI on score file
-Rscript enrichGI.R -i ${indir}/*masterTable.txt \
+Rscript enrichGI.R -i ${input_file} \
                    -a ${annotation_file} \
-                   -o ${outdir}
-
-# Combine all CrisprDB-style plots to single PDF via ImageMagick
-plots=$(ls */*/*dbstyle.png)
-convert ${plots} ${crispr_set}_residual_scatterplots.pdf
+                   --MIN_GENE 0 \
+                   --MAX_GENE 10000 \
+                   -o ${outdir} \
+                   -q 287
