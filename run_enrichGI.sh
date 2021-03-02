572bc4053bb0bff7247cba4093bc306e75878287
Catherine Ross <catherinem.ross@mail.utoronto.ca>
2021-03-02 17:30:46 -0500

    updating file paths

--- run_enrichGI.sh
+++ run_enrichGI.sh
@@ -2,30 +2,28 @@
 # Script to run enrichGI on specified set of gene scores (eg crispRscore output)
 
 # Define user parameters
-## 1) crispr_type: one of chemgen (chemical), musgen (mouse), mgin (mouse GI), abgen (antibody), or pace
-## 2) crispr_path: path to folder where gene scores are stored
-## 2) crispr_path: name of child folder where gene scores are stored
-## 3) anno_path: path to folder where annotation files are stored
-#crispr_type="mgin"
-#crispr_path="/Users/catherineross/projects/GIN/scripts/crispRscore/output"
-#crispr_set="20201102_MGIN"
-crispr_type="gin"
-crispr_path="/Users/catherineross/projects/qGI/output_data"
-crispr_set="20201028"
-anno_path="/Users/catherineross/projects/GIN/anno"
+## 1) input_type: one of chemgen (chemical), musgen (mouse), mgin (mouse GI), abgen (antibody), or pace
+## 2) input_set: name of child folder where gene scores are stored
+## 3) input_path: path to folder where gene scores are stored
+## 4) anno_path: path to folder where annotation files are stored
+input_type="gin"
+input_set="20210107"
+input_path="/Users/catherineross/data/qGI"
+anno_path="/Users/catherineross/anno"
 
 # I/O
 dt=`date +%Y%m%d`
-indir=${crispr_path}/${crispr_set}
-outdir=output/${crispr_set}
+indir=${input_path}/${input_set}
+outdir=output/${input_set}_${input_type}
 
 # Specify appropriate gene set annotation file for CRISPR screen type
-if [ $crispr_type = "musgen" ] || [ $crispr_type = "mgin" ]; then
+if [ $input_type = "musgen" ] || [ $input_type = "mgin" ]; then
   annotation_file=${anno_path}/MOUSE_GO_bp_no_GO_iea_symbol.gmt
   input_file=${indir}/*_masterTable.txt
 else
-  annotation_file=${anno_path}/Human_GO_bp_no_GO_iea_October_01_2018_symbol.gmt
-  input_file=${indir}/qGI_${crispr_set}.txt
+  #annotation_file=${anno_path}/Human_GO_bp_no_GO_iea_October_01_2018_symbol.gmt
+  annotation_file=${anno_path}/coreComplexes_Corum_Human_October_25_2018.gmt
+  input_file=${indir}/qGI_${input_set}.txt
 fi
 
 # Generate dated output directory
@@ -35,10 +33,10 @@ else
   mkdir $outdir
 fi
 
-# Run enrichGI on score file
+# Run enrichGI on given inputs
 Rscript enrichGI.R -i ${input_file} \
                    -a ${annotation_file} \
-                   --MIN_GENE 10 \
+                   --MIN_GENE 0 \
                    --MAX_GENE 500 \
                    -o ${outdir} \
-                   -q 287
+                   -q 149,307,335
